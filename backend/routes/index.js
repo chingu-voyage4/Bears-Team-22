27f3e4a1b11ac9d512d758a4bd467cc988d9c2f3
Luc Somers <lucwsomers@gmail.com>
2018-03-11 18:10:27 -0300

    initial schema

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 import employee from './users'
+import company from './companies'
 
 router.use('/employee', employee);
+router.use('/company', company);
 
 module.exports = router;
