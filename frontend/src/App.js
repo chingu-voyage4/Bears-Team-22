48baead02d7d5b6ca36d62d79624528506d9db77
Luc Somers <lucwsomers@gmail.com>
2018-03-11 18:44:00 -0300

    fiixed merge issues, added company schema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route , withRouter} from 'react-router-dom';
+import { Route, withRouter, Switch } from 'react-router-dom';
 
 
 import JobAdd from './components/jobs/JobAdd'
@@ -16,7 +16,7 @@ import './assets/css/App.css';
 // todo: pass this to redux?
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -31,20 +31,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <AppHeader className={this.state.headerClass}/>
-        <Route exact path="/" component={Home}/>
+      <Switch className="App">
+        <AppHeader className={this.state.headerClass} />
+        <Route exact path="/" component={Home} />
 
-        {/* set your routes here and add the className="app-route" to the components*/ }
-          <Route path="/jobs" component={Jobs}/>
+        {/* set your routes here and add the className="app-route" to the components*/}
+        <Route exact path="/jobs" component={Jobs} />
+        <Route path="/jobs/add" component={JobAdd} />
 
-         <JobAdd/>
 
-        <AppHeader/>
-        <Home/>
-        <AppFooter/>
+        <AppHeader />
+        <Home />
+        <AppFooter />
 
-      </div>
+      </Switch>
     );
   }
 
