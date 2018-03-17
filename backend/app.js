43cc642d2d9bf4809e4273c3a599c360abffd610
Jesus Guerrero <jesusant.guerrero@gmail.com>
2018-03-17 14:31:21 -0400

    add account login/auth (#43)
    all good and conflict result

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./db');
+import passport from './services/passport';
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
 import api from './routes';
 import schema from './graphql/schemas';
 import cors from 'cors';
 const app = express();
+app.use(session({
+	 secret: 'jabbatical-clone',
+	 resave: true ,
+	 saveUninitialized: true,
+	store: new MongoStore({mongooseConnection: db})
+ }));
 
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
+
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/graphql', graphqlExpress((req, res) => ({
+	 schema,
+	 rootValue: { session: req.session, user: req.user },
+	 graphiql: true,
+	 context: { req }
+ })));
 
 
 //Routes
---
+++ backend/graphql/resolvers/accountResolvers.js
@@ -0,0 +1,36 @@
+import Account from '../../models/Account';
+import passport from './../../services/passport';
+
+const AccountResolvers = {
+  Query: {
+    me(parent, args, context) {
+			if(context.sessionID) {
+				return context.user
+			}
+				return false
+		},
+
+		accounts() {
+			return Account.find(accunts => accounts)
+		}
+  },
+
+  Mutation: {
+      registerAccount(_, args, context) {
+				return Account.register( new Account({email: args.email, fullname: args.fullname}), args.password)
+			},
+
+			async login(_, args, context) {
+				const account = new Account({email: args.email});
+				const result = await account.authenticate( args.password);
+				return (result) ? result.user : {};
+			},
+
+			remove() {
+				return Account.remove({})
+			}
+  }
+}
+
+export default AccountResolvers
+
