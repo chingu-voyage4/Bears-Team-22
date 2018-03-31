dddd08200ef1d6c2dcaaa0250a46850b255bf86b
Jesus Guerrero <jesusant.guerrero@gmail.com>
2018-03-30 20:59:54 -0400

    fix: jobSchema typo

--- backend/graphql/resolvers/jobResolvers.js
+++ backend/graphql/resolvers/jobResolvers.js
@@ -1,31 +1,26 @@
-
-import jobModel from '../../models/Jobs';
-
-
+import jobModel from '../../models/jobs';
 
 export default {
   Query: {
     job(_, { id }) {
-      return jobModel.find({ id: id }, { "_id": 0, "id": 1, "positionName": 1 }).then(function (job) {
-        return job[0];
-      },
-        function (err) {
+      return jobModel.find({ id: id }, { _id: 0, id: 1, positionName: 1 }).then(
+        function(job) {
+          return job[0];
+        },
+        function(err) {
           console.log('cry error ', err);
         }
       );
     },
     jobs() {
-
-      return jobModel.find({}, { "_id": 0, "id": 1, "positionName": 1 }).then(function (job) {
-        return job;
-      },
-        function (err) {
+      return jobModel.find({}, { _id: 0, id: 1, positionName: 1 }).then(
+        function(job) {
+          return job;
+        },
+        function(err) {
           console.log('cry error ', err);
-
         }
       );
     }
   }
-}
-
-
+};
